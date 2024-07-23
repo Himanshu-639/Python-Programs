b7889b6bd967290b303904533c9466cf00a5c8ed
Himanshu Yadav <639himanshuyadavg@gmail.com>
2024-07-23 20:01:10 +0530

    Team Allocator

---
+++ Projects/Team_Allocator.py
@@ -0,0 +1,23 @@
+import random
+
+players = ["Ram", "Shyam", "Balram", "Laxman", "Radhe", "Rajni"]
+
+print("~ ~ ~ Welcome to Team Allocator ~ ~ ~")
+
+random.shuffle(players)
+team1 = players[:len(players)//2]
+cap1 = random.choice(team1)
+
+team2 = players[len(players)//2:]
+cap2 = random.choice(team2)
+
+print("Team 1 : ")
+print("Caption : " + cap1)
+for players in team1:
+    print(players)
+
+
+print("Team 2 : ")
+print("Caption : " + cap2)
+for players in team2:
+    print(players)
